57079bb43c8f692847a85d40614fe570b62daecd
mri-dev <pocokxtramepocokxtrame@gmail.com>
2016-11-04 13:25:01 +0100

    Save

--- web/application/views/v1.0/b2b/sub/beallitasok.php
+++ web/application/views/v1.0/b2b/sub/beallitasok.php
@@ -1,1 +1,20 @@
 <h1>Beállítások</h1>
+<?=$this->rmsg?>
+<div class="row">
+  <form action="" method="post">
+    <div class="col-md-6">
+      <h3>Kapcsolattartó adatok</h3>
+    </div>
+  </form>
+</div>
+<div class="divider"></div>
+<div class="row">
+  <form action="" method="post">
+    <div class="col-md-6">
+      <h3>Számlázási adatok</h3>
+    </div>
+    <div class="col-md-6">
+      <h3>Szállítási adatok</h3>
+    </div>
+  </form>
+</div>
